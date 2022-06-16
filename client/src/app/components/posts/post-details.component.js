25e375e0e034faca6f84d59ba4691df1ee08b552
Brent Van Steertegem <brent.vansteertegem@student.arteveldehs.be>
2022-06-16 18:06:25 +0200

    Added visual changes to post detail

--- client/src/app/components/posts/post-details.component.js
+++ client/src/app/components/posts/post-details.component.js
@@ -1,6 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import { useParams, NavLink } from 'react-router-dom';
-
+import { Card, CardBody, CardText, CardTitle } from 'reactstrap';
 
 const PostDetailsComponent = () => {
   const {id} = useParams();
@@ -32,12 +32,20 @@ const PostDetailsComponent = () => {
     if (error) return <p>{JSON.stringify(error)}</p>;
 
     return (
-      <div className="post-details">
-        <NavLink to={'/blog'}>Return to blog</NavLink>
-        <h1>{data.posts[0].title}</h1>
-        <p>{data.posts[0].description}</p>
-        <div className="" dangerouslySetInnerHTML={{ __html: data.posts[0].body.html }}>
-        </div>
+      <div>
+        <Card className='post_detail'>
+          <section className='post_detail__heading'>
+            <NavLink to={'/blog'}>Return to blog</NavLink>
+            <span>
+              <img className='post_detail__avatar' src={data.posts[0].authUser.profile.avatar} alt={`${data.posts[0].authUser.username}'s avatar`}/>
+              <span>{data.posts[0].authUser.username}</span>
+            </span>
+          </section>
+          <CardBody>
+            <CardTitle><h1>{data.posts[0].title}</h1></CardTitle>
+            <CardText dangerouslySetInnerHTML={{ __html: data.posts[0].body.html }}></CardText>
+          </CardBody>
+        </Card>
       </div>
     )
   };
